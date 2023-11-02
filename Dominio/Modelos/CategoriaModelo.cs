d6316c31baf4e631fb3fadb8b4161fdf42d86f8f
kportella <portellakaue@gmail.com>
2023-11-02 20:47:04 -0300

    Criando modelos.

---
+++ Dominio/Modelos/CategoriaModelo.cs
@@ -0,0 +1,8 @@
+﻿namespace Dominio.Modelos
+{
+    public class CategoriaModelo
+    {
+        public Guid Id { get; set; }
+        public string Categoria { get; set; }
+    }
+}
---
+++ Dominio/Modelos/TransacaoModelo.cs
@@ -0,0 +1,16 @@
+﻿namespace Dominio.Modelos
+{
+    public class TransacaoModelo
+    {
+        public Guid Id { get; set; }
+        public bool Tipo {  get; set; }
+        public Guid CategoriaId { get; set; }
+        public string Descricao {  get; set; }
+        public DateTime Data { get; set; }
+        public float Valor { get; set; }
+
+        // Navigation properties
+
+        public CategoriaModelo Categoria { get; set; }
+    }
+}
