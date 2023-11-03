f7aba206d214e46c9d1b12d4fb2d0564a82a7ec1
kportella <portellakaue@gmail.com>
2023-11-02 22:26:17 -0300

    criado TransacaoController

---
+++ ControleGastos/Controllers/TransacaoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleGastos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransacaoController : Controller
+    {
+        [HttpGet]
+        public IActionResult BuscarTodos() { return View(); }
+
+        [HttpGet]
+        public IActionResult BuscarPorId() { return View(); }
+
+        [HttpPost]
+        public IActionResult Adicionar() { return View(); }
+
+        [HttpPut]
+        public IActionResult Atualizar() { return View(); }
+
+        [HttpDelete]
+        public IActionResult Deletar() { return View(); }
+        
+    }
+}
